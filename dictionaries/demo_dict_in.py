53f1874336af0d16d2aa209bd9ee333a536ff8ff
Rasul Sharifzade <rasul.sharifzade@gmail.com>
2020-03-20 13:20:31 +0400

    learned true in

--- dictionaries/demo_dict_in.py
+++ dictionaries/demo_dict_in.py
@@ -3,5 +3,5 @@ thisdict = {
     "model":"mustang",
     "year":1964
 }
-if model in thisdict:
-    print('yes, 'model' is one  of the keys in the thisdict')
+if "model" in thisdict:
+    print('yes, "model" is one  of the keys in the thisdict')
