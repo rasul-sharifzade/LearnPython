0870d5bc23f2e697c7ae47fda0b4bb2c1c00a654
Rasul Sharifzade <rasul.sharifzade@gmail.com>
2020-03-24 15:47:12 +0400

    learned true or

--- if...else/demo_if_or.py
+++ if...else/demo_if_or.py
@@ -1,5 +1,5 @@
- a = 200
- b = 33
- c = 500
- if a > b  or a > c:
-    print("at leaast one of condirions is true")
+a = 200
+b = 33
+c = 500
+if a > b  or a > c:
+   print("at leaast one of condirions is true")
