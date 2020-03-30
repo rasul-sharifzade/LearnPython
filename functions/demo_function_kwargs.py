718096a81b6984642e1bf01a03b53ef8dcf9187a
Rasul Sharifzade <rasul.sharifzade@gmail.com>
2020-03-30 18:20:50 +0400

    learned true func args

--- functions/demo_function_kwargs.py
+++ functions/demo_function_kwargs.py
@@ -1,3 +1,4 @@
-def my_function(child3, child2, demo_function_kwargschild1):
+zzzzdef my_function(child3, child2, child1):
   print("The youngest child is " + child3)
+
 my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
