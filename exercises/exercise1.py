c0235ce551d9735a075668b15195f7bf5c3bf6b8
Rasul Sharifzade <rasul.sharifzade@gmail.com>
2020-05-04 00:04:16 +0400

    exercises

---
+++ exercises/exercise1.py
@@ -0,0 +1,7 @@
+def findmax(r,k,y):
+   if r < k < y:
+       print(y)
+   else:
+       print(k)
+
+findmax(2,4,6)
---
+++ exercises/exercise2.py
@@ -0,0 +1,6 @@
+thislist = [2,3,4,5,6]
+def listSummer(r,u,t,y,f):
+    print(r + u + t + y + f)
+
+
+listSummer(2,3,4,5,6)
